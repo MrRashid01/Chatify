66828b022c31597d43ad5809f966f54987e7bea4
MrRashid01 <rk.rashidkhanqwe@gmail.com>
2026-07-13 16:25:45 +0530

    node engine updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,17 +3,27 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 
 dotenv.config();
 
 const app=express();
+const __dirname= path.resolve();
 
 const PORT=process.env.PORT ||4000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"Frontend","dist","index.html"))
+    })
+}   
+
 app.listen(PORT,()=>{
-    console.log("Server running on port "+PORT)
+    console.log("Server running on port "+ PORT)     
 })
