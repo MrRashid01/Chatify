0ad6d08d0114f9cad7d83c9fc35113b108d9ddc9
MrRashid01 <rk.rashidkhanqwe@gmail.com>
2026-07-15 15:04:39 +0530

    login logout functionality

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -59,3 +59,36 @@ export const signup=async(req,res)=>{
         return res.status(500).json({message:"Server error"});
     }
 }
+
+export const login=async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email || !password){
+        return res.status(400).json({message:"Please provide both email and password"});
+    }
+
+    try{
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+        generateToken(user._id,res);
+        return res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic,
+        });
+    }catch(error){
+        console.error("Error during login:",error);
+        return res.status(500).json({message:"Server error"});
+    }
+}
+
+export const logout=(_,res)=>{
+     res.cookie("jwt","",{maxAge:0});
+     return res.status(200).json({message:"Logged out successfully"});
+}
